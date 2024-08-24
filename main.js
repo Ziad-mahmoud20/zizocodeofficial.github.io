a2d2b031bc2b1b71c12e78e62c7edf85f43c4e22
Ziad-mahmoud20 <ziadmahmoudd73@gmail.com>
2024-08-24 03:38:22 +0300

    Added new files

---
+++ main.js
@@ -0,0 +1,36 @@
+var btnvar1 = document.getElementById('btnh1');
+function Toggle1(){
+    if (btnvar1.style.color =="red") {
+        btnvar1.style.color = "gray"
+    }
+    else{
+        btnvar1.style.color = "red"
+    }
+}
+var btnvar2 = document.getElementById('btnh2');
+function Toggle2(){
+    if (btnvar2.style.color =="red") {
+        btnvar2.style.color = "gray"
+    }
+    else{
+        btnvar2.style.color = "red"
+    }
+}
+var btnvar3 = document.getElementById('btnh3');
+function Toggle3(){
+    if (btnvar3.style.color =="red") {
+        btnvar3.style.color = "gray"
+    }
+    else{
+        btnvar3.style.color = "red"
+    }
+}
+var btnvar4 = document.getElementById('btnh4');
+function Toggle4(){
+    if (btnvar4.style.color =="red") {
+        btnvar4.style.color = "gray"
+    }
+    else{
+        btnvar4.style.color = "red"
+    }
+}
